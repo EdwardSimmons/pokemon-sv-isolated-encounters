8d8f0dbb4d290aff91b852af508d04e427f0d311
Ed Simmons <e.d.simmons@outlook.com>
2024-04-19 09:59:17 +0100

    Fix: kudos

--- src/Kudos.tsx
+++ src/Kudos.tsx
@@ -3,7 +3,17 @@ import { Box, Link, Typography } from "@mui/material"
 export default function Kudos() {
   return (
     <Box>
-      <Typography sx={{ mt: { xs: 2, md: 0 } }}>
+      <Typography sx={{ mt: { xs: 3, md: 2 } }}>
+        The term "isloated encounter shiny hunting" was coined by{" "}
+        <Link
+          target="_blank"
+          href="https://www.youtube.com/watch?v=y9zO9LiFF2Y&t=804s&ab_channel=AustinJohnPlays"
+        >
+          AustinJohnPlays
+        </Link>
+        . You should definitely check out their channel.
+      </Typography>
+      <Typography sx={{ mt: { xs: 2, md: 1 } }}>
         This project was inspired by the one created by{" "}
         <Link
           target="_blank"
@@ -13,14 +23,14 @@ export default function Kudos() {
         </Link>
         .
       </Typography>
-      <Typography sx={{ mt: { xs: 2, md: 0 } }}>
+      <Typography sx={{ mt: { xs: 2, md: 1 } }}>
         All of the necessary data was obtained from{" "}
         <Link target="_blank" href="https://www.serebii.net">
           Serebii.net
         </Link>
         .
       </Typography>
-      <Typography sx={{ mt: { xs: 2, md: 0 } }}>
+      <Typography sx={{ mt: { xs: 2, md: 1 } }}>
         I recommend using{" "}
         <Link target="_blank" href="https://birbzone.com/sandwich/">
           this tool
